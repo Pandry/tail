7e5ee3bbe4159dab163f0a8e5790b81f912acd29
Mezzi Sotoodeh <mehrzad55@gmail.com>
2018-04-19 12:53:05 -0700

    added line num to Line struct
    fixed zero line number bug and added unit tests
    
    removed unnecessary StopAtEOF
    
    include line num count for split lines

--- tail.go
+++ tail.go
@@ -16,9 +16,15 @@ import (
 	"sync"
 	"time"
 
+<<<<<<< HEAD
 	"github.com/admpub/tail/ratelimiter"
 	"github.com/admpub/tail/util"
 	"github.com/admpub/tail/watch"
+=======
+	"github.com/hpcloud/tail/ratelimiter"
+	"github.com/hpcloud/tail/util"
+	"github.com/hpcloud/tail/watch"
+>>>>>>> added line num to Line struct
 	tomb "gopkg.in/tomb.v1"
 )
 
@@ -28,13 +34,14 @@ var (
 
 type Line struct {
 	Text string
+	Num  int
 	Time time.Time
 	Err  error // Error from tail
 }
 
 // NewLine returns a Line with present time.
-func NewLine(text string) *Line {
-	return &Line{text, time.Now(), nil}
+func NewLine(text string, lineNum int) *Line {
+	return &Line{text, lineNum, time.Now(), nil}
 }
 
 // SeekInfo represents arguments to `os.Seek`
@@ -84,8 +91,9 @@ type Tail struct {
 	Lines    chan *Line
 	Config
 
-	file   *os.File
-	reader *bufio.Reader
+	file    *os.File
+	reader  *bufio.Reader
+	lineNum int
 
 	watcher watch.FileWatcher
 	changes *watch.FileChanges
@@ -368,6 +376,8 @@ func (tail *Tail) closeFile() {
 
 func (tail *Tail) reopen() error {
 	tail.closeFile()
+	// reset line number
+	tail.lineNum = 0
 	for {
 		var err error
 		tail.file, err = OpenFile(tail.Filename)
@@ -466,7 +476,7 @@ func (tail *Tail) tailFileSync() {
 				// file when rate limit is reached.
 				msg := ("Too much log activity; waiting a second " +
 					"before resuming tailing")
-				tail.Lines <- &Line{msg, time.Now(), errors.New(msg)}
+				tail.Lines <- &Line{msg, tail.lineNum, time.Now(), errors.New(msg)}
 				select {
 				case <-time.After(time.Second):
 				case <-tail.Dying():
@@ -609,7 +619,8 @@ func (tail *Tail) sendLine(line string) bool {
 	}
 
 	for _, line := range lines {
-		tail.Lines <- &Line{line, now, nil}
+		tail.lineNum++
+		tail.Lines <- &Line{line, tail.lineNum, now, nil}
 	}
 
 	if tail.Config.RateLimiter != nil {
